7289f33a72631c55becdad5914c41120ccb42726
rafael campos <rafaelcc1511@gmail.com>
2026-05-05 23:34:20 -0600

    Mejora de listado de artwork

--- marketplace/models.py
+++ marketplace/models.py
@@ -16,7 +16,7 @@ class Artwork(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    image = models.ImageField(upload_to='artworks/')
+    image = models.ImageField(upload_to='artworks/', null=True, blank=True)
     stock = models.IntegerField(default=1)
     is_available = models.BooleanField(default=True)
 

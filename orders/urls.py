d9b501b88f336ce0afdcd3e26909e14af4cab482
rafael campos <rafaelcc1511@gmail.com>
2026-05-05 23:49:50 -0600

    Se agregó listado de ordenes

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('buy/<int:pk>/', views.create_order, name='create_order'),
     path('<int:pk>/', views.order_detail, name='order_detail'),
+    path('', views.order_list, name='order_list'),
 ]

86d53b568e18bd6283e8f3692302b0f51e2afe0f
rafael campos <rafaelcc1511@gmail.com>
2026-05-10 22:06:05 -0600

    Vista de obras publicadas

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.artwork_list, name='artwork_list'),
     path('art/<int:pk>/', views.artwork_detail, name='artwork_detail'),
     path('create/', views.create_artwork, name='create_artwork'),
+    path('my-artworks/', views.my_artworks, name='my_artworks'),
 ]
